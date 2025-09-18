ccde655a3d24dbe88eb4814bd0531417bb0bdd78
L1ttleDodo <do.nat.cvn@gmail.com>
2025-09-18 10:15:55 +0300

    lab1 1.1

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -5,9 +5,17 @@
 
 int main(int argc, char * argv[]) {
     std::string line;
+    int size;
 
-    std::cout << "Введите строку для обработки" << std::endl;
+    std::cout << "Введите стоку для обработки (длина строки от 0 до 100 символов)" << std::endl;
     std::cin >> line; 
+
+    size = line.length();
+
+    if (size > 100) {
+        std::cout << "Строка превосходи ограничения!" << std::endl;
+        return 0;
+    }
 
     std::cout << std::boolalpha;
     std::cout << "Результат: " << is_valid(line) << std::endl;
